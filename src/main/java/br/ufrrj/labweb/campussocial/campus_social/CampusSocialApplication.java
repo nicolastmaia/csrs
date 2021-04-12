63dc3f62839a5b113297fbfd97e954412e16bb26
Lucas Vinicius <vinicius.gom3@gmail.com>
2021-04-12 18:25:01 -0300

    Initial commit

---
+++ src/main/java/br/ufrrj/labweb/campussocial/campus_social/CampusSocialApplication.java
@@ -0,0 +1,13 @@
+package br.ufrrj.labweb.campussocial.campus_social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufrrj/labweb/campussocial/campus_social/CampusSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrrj.labweb.campussocial.campus_social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
