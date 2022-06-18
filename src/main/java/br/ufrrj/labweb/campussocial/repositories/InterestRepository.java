13f3c0ccf3930e94b60505aa2b5aca6299888d7f
Nícolas Maia <nicolasterramaia@gmail.com>
2022-06-17 21:16:14 -0300

    fix: recognize InterestRepository

--- src/main/java/br/ufrrj/labweb/campussocial/repositories/InterestRepository.java
+++ src/main/java/br/ufrrj/labweb/campussocial/repositories/InterestRepository.java
@@ -5,7 +5,9 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class InterestRepository {
 
   @Autowired
